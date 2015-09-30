1550ad63443893c44fb72341829332f14250a681
Monro64 <conorm776@gmail.com>
2015-09-30 15:58:08 +0000

    Third attempt, a syntax error that was quite frankly embarrassing was my downfall. Need to familiarize with Python.

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -1,14 +1,14 @@
-#Second version of code, does not work. Wanted to try shorter comparison.
-word = "Oxo" # first test for predefined word
+#Third test, a glaringly obvious syntax error turned out to be my downfall.
+word = "oxo" # first test for predefined word
 wordRev = word[::-1] #reverses original string, stores in new variable
 #Counter removed, no need for it.
 
 if (word == wordRev): #This should work, considering if it is a palindrome, they will simply be identical.
   {
-    print "True"
+    print ("True")
   }
 
-else: #It did not.
+else: #It worked somewhat. Case handling to be added in final version
   {
-    print "False"
+    print ("False")
   }
