86d84926f7a35e27fe77f4afe3335e24e10490a3
Monro64 <conorm776@gmail.com>
2015-09-30 16:20:40 +0000

    Final version, no change to code. Appended a few comments.

--- Lab3/palindrome.py
+++ Lab3/palindrome.py
@@ -4,11 +4,12 @@ word = word.lower() #Removed case sensitivity, works for any and all strings.
 wordRev = word[::-1] #reverses original string, stores in new variable
 #Counter removed, no need for it.
 
-if (word == wordRev): #This should work, considering if it is a palindrome, they will simply be identical.
+if (word == wordRev): #Compares current string to reversed one. Will work no matter what, regardless of characters
+  #or spaces present in the input string.
   {
     print ("True")
   }
-else: #It worked somewhat. Case handling to be added in final version
+else:
   {
     print ("False")
   }
